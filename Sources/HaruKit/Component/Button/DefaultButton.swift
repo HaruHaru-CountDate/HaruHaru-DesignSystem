66eac009d6e0c884c4826fd3eaff902cdfbcc076
김은찬 <164847037+eunchan2815@users.noreply.github.com>
2025-05-13 13:45:28 +0900

    Update DefaultButton.swift

--- Sources/HaruKit/Component/Button/DefaultButton.swift
+++ Sources/HaruKit/Component/Button/DefaultButton.swift
@@ -12,6 +12,11 @@ public struct DefaultButton: View {
     let title: String
     let action: () -> Void
     
+    public init(title: String, action: @escaping () -> Void) {
+        self.title = title
+        self.action = action
+    }
+    
     public var body: some View {
         Button {
             action()
